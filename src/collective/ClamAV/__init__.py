e2f8aae8b944b66d827caeb36e2e903e23ef780e
andreasma <maand@gmx.de>
2016-05-30 10:03:44 +0200

    Adding validation.

--- src/collective/ClamAV/__init__.py
+++ src/collective/ClamAV/__init__.py
@@ -2,5 +2,10 @@
 """Init and utils."""
 
 from zope.i18nmessageid import MessageFactory
+from Products.validation import validation
+
 
 _ = MessageFactory('collective.ClamAV')
+
+from collective.ClamAV.validator import ClamAVValidator
+validation.register(ClamAVValidator('isVirusFree'))
